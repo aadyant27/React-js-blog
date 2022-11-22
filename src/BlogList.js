606fc67cb00fce59cd99f5f42125661d303bf957
Aadyant <aadyant2017@gmail.com>
2022-11-22 21:38:21 +0530

    Minor Changes

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,12 +1,14 @@
 const BlogList = (props) => {
-  console.log(props.blogs);
-  const blog = props.blogs;
+  const blog = props.blog;
+  const handleDelete = props.handleDelete;
+
   return (
     <div className="blog-list">
       {blog.map((blog) => (
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written By: {blog.author}</p>
+          <button onClick={() => handleDelete(blog.id)}> Delete Blog </button>
         </div>
       ))}
     </div>
--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blog, setBlog] = useState([
+  const [blogs, setBlogs] = useState([
     { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
     { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
     {
@@ -12,9 +12,14 @@ const Home = () => {
       id: 3,
     },
   ]);
+  const handleDelete = (id) => {
+    //filer method does not change the original array but creates a copy of the orginal array based on the filter-property
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
   return (
     <div className="home">
-      <BlogList blogs={blog} />
+      <BlogList blog={blogs} handleDelete={handleDelete} />
       <h2>Welcome to the page</h2>
     </div>
   );
